00778fded7de2b6a446e0f4dfbef73522e008cad
Ker Tenepere <Ker10epere@gmail.com>
2024-03-09 17:23:53 +0800

    add CustomErrorResponse.timestamp format

--- src/main/java/com/withintegrity/validationanderrorhandling/controller/CustomErrorResponse.java
+++ src/main/java/com/withintegrity/validationanderrorhandling/controller/CustomErrorResponse.java
@@ -1,5 +1,6 @@
 package com.withintegrity.validationanderrorhandling.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.annotation.Nullable;
 import org.springframework.http.HttpStatusCode;
@@ -13,6 +14,7 @@ public record CustomErrorResponse(
         HttpStatusCode statusCode,
         String title,
         String description,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
         ZonedDateTime timestamp,
         @Nullable
         List<AbstractMap.SimpleEntry<String, String>> parameters
