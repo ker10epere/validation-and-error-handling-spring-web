1be823bc8485fd6b4a434d988333932b8e89c7b6
Ker Tenepere <Ker10epere@gmail.com>
2024-03-09 11:48:57 +0800

    init commit

---
+++ src/main/java/com/withintegrity/validationanderrorhandling/ValidationAndErrorHandlingSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.withintegrity.validationanderrorhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationAndErrorHandlingSpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationAndErrorHandlingSpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/withintegrity/validationanderrorhandling/ValidationAndErrorHandlingSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withintegrity.validationanderrorhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationAndErrorHandlingSpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
