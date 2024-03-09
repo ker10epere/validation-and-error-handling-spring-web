6f6ff1fee155b2b4a902b60606872cde1758598c
Ker Tenepere <Ker10epere@gmail.com>
2024-03-09 22:50:14 +0800

    print logs with slf4j

--- src/main/java/com/withintegrity/validationanderrorhandling/controller/PetController.java
+++ src/main/java/com/withintegrity/validationanderrorhandling/controller/PetController.java
@@ -1,14 +1,17 @@
 package com.withintegrity.validationanderrorhandling.controller;
 
 import com.withintegrity.validationanderrorhandling.model.Pet;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PetController {
     @PostMapping("/pet")
     public void addPet(@RequestBody Pet pet) {
-        System.out.println("> PetController.addPet " + pet);
+        if (log.isDebugEnabled())
+            log.debug("> PetController.addPet " + pet);
     }
 }
--- src/main/java/com/withintegrity/validationanderrorhandling/controller/PetRequestAdvice.java
+++ src/main/java/com/withintegrity/validationanderrorhandling/controller/PetRequestAdvice.java
@@ -3,38 +3,45 @@ package com.withintegrity.validationanderrorhandling.controller;
 import com.withintegrity.validationanderrorhandling.model.Pet;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
 
-import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice(assignableTypes = PetController.class)
-public class PetRequestAdvice implements RequestBodyAdvice {
+public class PetRequestAdvice extends RequestBodyAdviceAdapter {
     private final Validator validator;
 
     @Override
     public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
-        System.out.println("> PetRequestAdvice.supports " + targetType.getTypeName().equals(Pet.class.getTypeName()));
-        return targetType.getTypeName().equals(Pet.class.getTypeName());
+        if (log.isDebugEnabled()) {
+            log.debug("> methodParameter.hasMethodAnnotation(PostMapping.class) " + methodParameter.hasMethodAnnotation(PostMapping.class));
+            log.debug("> methodParameter " + methodParameter);
+            log.debug("> converterType " + converterType);
+            log.debug("> targetType " + targetType);
+            log.debug("> Pet.class.getTypeName() " + Pet.class.getTypeName());
+            log.debug("> Pet.class.getTypeName().equals(targetType.getTypeName()) " + Pet.class.getTypeName().equals(targetType.getTypeName()));
+        }
+        return Pet.class.getTypeName().equals(targetType.getTypeName());
     }
 
-    @Override
-    public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
-        return inputMessage;
-    }
 
     @Override
     public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
         Pet pet = (Pet) body;
-        System.out.println("> PetRequestAdvice.afterBodyRead body " + pet);
+        if (log.isDebugEnabled()) {
+            log.debug("> PetRequestAdvice.afterBodyRead body " + pet);
+        }
         Set<ParameterViolation> violations = validator.validate(pet)
                 .stream()
                 .map(violation -> new ParameterViolation(violation.getPropertyPath().toString(), violation.getMessage()))
@@ -44,11 +51,6 @@ public class PetRequestAdvice implements RequestBodyAdvice {
             throw new PetValidationResponseException(HttpStatus.BAD_REQUEST, violations);
         }
 
-        return body;
-    }
-
-    @Override
-    public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
         return body;
     }
 
