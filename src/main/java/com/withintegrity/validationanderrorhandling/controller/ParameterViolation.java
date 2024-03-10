9a2ad42cced84b03bff88a28802494c80b8223c2
ker10epere <60257991+ker10epere@users.noreply.github.com>
2024-03-10 22:53:32 +0800

    Update ParameterViolation.java
    add compareTo comments

--- src/main/java/com/withintegrity/validationanderrorhandling/controller/ParameterViolation.java
+++ src/main/java/com/withintegrity/validationanderrorhandling/controller/ParameterViolation.java
@@ -5,6 +5,9 @@ import java.util.Comparator;
 public record ParameterViolation(String parameter, String message) implements Comparator<ParameterViolation> {
     @Override
     public int compare(ParameterViolation o1, ParameterViolation o2) {
+        // COMPARE STRINGS ALPHABETICALLY
+        // String.compareTo() BEHAVES UNEXPECTEDLY WITH DIFFERENT CASES (e.g. compareTo ["abCd","Abcd1"] -> ["Abcd1","abCd"])
+        // USE String.compareToIgnoreCase() TO IGNORE RANDOM CASING OF STRINGS. (e.g. compareToIgnoreCase ["abCd","Abcd1"] -> ["abCd", "Abcd1"])
         return o1.parameter.compareTo(o2.parameter);
     }
 }
